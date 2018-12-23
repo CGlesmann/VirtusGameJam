ca0eaa07b960df6430f14129d3a8317b2bdfa587
LunaTicode <mak98k@gmail.com>
2018-12-23 02:44:57 +0200

    Add inventory

---
+++ Assets/Tools/Scripts/{}Common/{}Inventory/{}Views/InventoryItemTooltipView.cs
@@ -0,0 +1,52 @@
+﻿/* Created by Luna.Ticode */
+
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using TMPro;
+
+public class InventoryItemTooltipView : View
+{
+	[SerializeField] private TextMeshProUGUI _descriptionTextField;
+
+	[SerializeField] private Vector2 _offset;
+
+	public void Display(InventoryItemSharedData inventoryItemSharedData, InventoryItemSlotView inventoryItemSlotView)
+	{
+		this._descriptionTextField.text = inventoryItemSharedData._Description;
+		
+		this.rectTransform.anchoredPosition = inventoryItemSlotView._RectTransform.anchoredPosition + this._offset;
+	}
+
+#if UNITY_EDITOR
+#endif
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(InventoryItemTooltipView))]
+[CanEditMultipleObjects]
+public class InventoryItemTooltipViewEditor : Editor
+{
+#pragma warning disable 0219, 414
+	private InventoryItemTooltipView _sInventoryItemTooltipView;
+#pragma warning restore 0219, 414
+
+	private void OnEnable()
+	{
+		this._sInventoryItemTooltipView = this.target as InventoryItemTooltipView;
+	}
+
+	public override void OnInspectorGUI()
+	{
+		this.DrawDefaultInspector();
+	}
+}
+#endif
