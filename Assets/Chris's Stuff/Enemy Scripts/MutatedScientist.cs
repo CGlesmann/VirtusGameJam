231e3e1c3c420450fff9a3b1bd03321a246073a0
LightAxe2402 <chrisglesmann@gmail.com>
2018-12-25 17:15:03 -0800

    MutatedScientist_JumpAttack

--- Assets/Chris's Stuff/Enemy Scripts/MutatedScientist.cs
+++ Assets/Chris's Stuff/Enemy Scripts/MutatedScientist.cs
@@ -2,23 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEditor;
 
 public class MutatedScientist : MonoBehaviour
 {
+    // Private Enums
+    private enum BossStage { Stage1, Stage2, Stage3 };
+
     [Header("MiniBoss Stats")]
     public UnitStats mbStats;
     public UnitState uState;
+    public GameObject player;
+    [SerializeField] private BossStage stage;
 
     [Header("Battle Start Variables")]
     public bool battleStarted = false;
     public Vector2 slamPosition;
 
-    [Header("Battle Ability Variables")]
+    [Header("Jump Ability Variables")]
     // Jumping Variables
-    [SerializeField] private bool isJumping;
+    [SerializeField] private GameObject shadow;
+    [SerializeField] private Vector2 shadowOffset;
+    [SerializeField] private Vector2 shadowScaleStart;
+    [SerializeField] private Vector2 shadowScaleEnd;
+    [SerializeField] private bool inAir; // Tracks when jump should be maintained
+    [SerializeField] private bool isJumping; // Tracks when jumping up
 
-    [Header("Test")]
-    public UnityEvent[] events;
+    [SerializeField] private float slamRadius;
+    [SerializeField] private float slamKnockback;
+
+    [Header("Starting Routine")]
+    public UnityEvent[] startEvents;
+
+    [Header("Phase 1")]
+    public UnityEvent[] phase1Events;
 
     private UnitMovement mControl;
 
@@ -26,37 +43,158 @@ public class MutatedScientist : MonoBehaviour
     {
         mControl = GetComponent<UnitMovement>();
         uState = new UnitState();
+
+        if (player == null)
+            player = GameObject.Find("Player");
+
+        shadow.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            if (!inAir && !isJumping)
+                JumpUp();
     }
 
     #region Start Functions
+    /// <summary>
+    /// Call this to function to start miniboss battle
+    /// </summary>
     public void StartBattle()
     {
-        //StartCoroutine("StartBattleRoutine");
+        StartCoroutine("StartBattleRoutine");
         return;
     }
 
-    #endregion
+    /// <summary>
+    /// Runs through the StartRoutine List
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator StartBattleRoutine()
+    {
+        // Declaring the tracker variables
+        float delay = 2f; // In Seconds
 
+        // Running Through Each Event
+        for(int i = 0; i < startEvents.Length; i++)
+        {
+            startEvents[i].Invoke();
+            yield return new WaitForSeconds(delay);
+        }
+
+        battleStarted = true;
+    }
+    #endregion
+    
     #region Ability Functions
+    public void JumpUp()
+    {
+        if (!isJumping && !inAir)
+        {
+            isJumping = true;
+            StartCoroutine("JumpingUp");
+        } else
+            Debug.Log("Trying to jump but cant: " + isJumping.ToString() + " | " + inAir.ToString());
+    }
+
+    public float GettingJumpingPosition()
+    {
+        return ((Camera.main.gameObject.transform.position.y + Camera.main.orthographicSize / 2f) + (transform.localScale.y * 2));
+    }
+
     IEnumerator JumpingUp()
     {
-        float yGoal = Camera.main.rect.yMax;
-        float time = 0.75f;
+        float yGoal = GettingJumpingPosition();
+        float time = 0.15f;
         int reps = 20;
         float inc = (yGoal - transform.position.y) / reps;
         float delay = (time / reps);
 
-        Debug.Log("yGoal: " + yGoal);
+        shadow.SetActive(true);
+        shadow.transform.position = transform.position + new Vector3(shadowOffset.x, shadowOffset.y);
+        shadow.transform.localScale = shadowScaleStart;
+
+        Vector2 shadowScaleInc = (shadowScaleEnd - shadowScaleStart) / reps;
 
         for (int i = 0; i < reps; i++)
         {
             Vector3 newPos = new Vector3(transform.position.x, transform.position.y + inc, transform.position.z);
             transform.position = newPos;
 
+            shadow.transform.localScale += new Vector3(shadowScaleInc.x, shadowScaleInc.y);
+
             yield return new WaitForSeconds(delay);
         }
 
-        isJumping = true;
+        yield return new WaitForSeconds(1f);
+
+        isJumping = false;
+        inAir = true;
+        StartCoroutine("FloatingInAir");
+    }
+
+    IEnumerator FloatingInAir()
+    {
+        // Declaring Tracking Variables
+        float time = 1.5f;
+        int reps = 200;
+        float delay = time / reps;
+        Vector2 floatPos;
+
+        // Repeatedly getting the float position
+        for(int i = 0; i < reps; i++)
+        {
+            floatPos = new Vector2(player.transform.position.x, GettingJumpingPosition());
+            transform.position = floatPos;
+            shadow.transform.position = player.transform.position + new Vector3(shadowOffset.x, shadowOffset.y);
+            yield return new WaitForSeconds(delay);
+        }
+
+        yield return new WaitForSeconds(0.75f);
+        StartCoroutine("SlamDown");
+    }
+
+    IEnumerator SlamDown()
+    {
+        float yGoal = shadow.transform.position.y - shadowOffset.y;
+        float time = 0.075f;
+        int reps = 20;
+        float inc = (yGoal - transform.position.y) / reps;
+        float delay = (time / reps);
+
+        shadow.transform.localScale = shadowScaleEnd;
+        Vector2 shadowScaleInc = (shadowScaleStart - shadowScaleEnd) / reps;
+
+        for (int i = 0; i < reps; i++)
+        {
+            Vector3 newPos = new Vector3(transform.position.x, transform.position.y + inc, transform.position.z);
+            transform.position = newPos;
+
+            shadow.transform.localScale += new Vector3(shadowScaleInc.x, shadowScaleInc.y);
+
+            yield return new WaitForSeconds(delay);
+        }
+
+        shadow.SetActive(false);
+        inAir = false;
     }
     #endregion
+
+}
+
+[CustomEditor(typeof(MutatedScientist))]
+public class BossInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        MutatedScientist s = (MutatedScientist)target;
+
+        if (GUILayout.Button("Jump Up Position"))
+        {
+            Debug.Log(s.GettingJumpingPosition());
+        }
+    }
 }
