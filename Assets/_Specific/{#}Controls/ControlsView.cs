06bfc8f22d5cee19c4e2cf1d3109ed2cf8ecc266
LunaTicode <mak98k@gmail.com>
2019-01-04 18:28:25 +0200

    Add exit menu, controls menu.

---
+++ Assets/_Specific/{#}Controls/ControlsView.cs
@@ -0,0 +1,49 @@
+﻿/* Created by Luna.Ticode */
+
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using TMPro;
+
+public class ControlsView : View
+{
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.F1))
+		{
+			this.ToggleVisibility();
+		}
+	}
+
+#if UNITY_EDITOR
+	//protected override void OnDrawGizmos()
+	//{
+	//}
+#endif
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(ControlsView))]
+[CanEditMultipleObjects]
+public class ControlsViewEditor : ViewEditor
+{
+#pragma warning disable 0219, 414
+	private ControlsView _sControlsView;
+#pragma warning restore 0219, 414
+
+	protected override void OnEnable()
+	{
+		base.OnEnable();
+
+		this._sControlsView = this.target as ControlsView;
+	}
+}
+#endif
